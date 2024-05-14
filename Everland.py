5746c99dd13c78e001f0ea56566d0b96bbcc3080
Philipp <github@mrvenerabilis.de>
2024-05-14 18:27:42 +0200

    Update Everland.py

--- Everland.py
+++ Everland.py
@@ -2,8 +2,19 @@ import requests
 import json
 import openmeteo_requests
 import requests_cache
-import pandas as pd
 from retry_requests import retry
+import statistics
+
+
+allowedDeviationPercentage = 10
+seaLevelRise = 3
+
+
+def getTopTenMedian(pData):
+    topTen = sorted(pData, reverse=True)[:10]
+    median = statistics.median(topTen)
+
+    return round(median, 3)
 
 
 # Setup the Open-Meteo API client with cache and retry on error
@@ -12,22 +23,63 @@ retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
 
-def getClimateData():
-    # Make sure all required weather variables are listed here
-    # The order of variables in hourly or daily is important to assign them correctly below
+def getLegacyClimateData(pLat, pLong):
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": pLat,
+        "longitude": pLong,
+        "start_date": "2020-01-01",
+        "end_date": "2020-12-31",
+        "daily": ["temperature_2m_max", "precipitation_sum", "wind_speed_10m_max"],
+        "timezone": "Europe/Berlin"
+    }
+
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+    daily = response.Daily()
+
+    temperature_2m_max = getTopTenMedian(response.Daily().Variables(0).ValuesAsNumpy())
+    precipitation_sum = getTopTenMedian(response.Daily().Variables(1).ValuesAsNumpy())
+    wind_speed_10m_max = getTopTenMedian(response.Daily().Variables(2).ValuesAsNumpy())
+
+    return temperature_2m_max, precipitation_sum, wind_speed_10m_max
+
+
+def getFutureClimateData(pLat, pLong):
     url = "https://climate-api.open-meteo.com/v1/climate"
     params = {
-        "latitude": 52.52,
-        "longitude": 13.41,
+        "latitude": pLat,
+        "longitude": pLong,
         "start_date": "2050-01-01",
         "end_date": "2050-12-31",
         "models": ["MRI_AGCM3_2_S"],
-        "daily": "pressure_msl_mean"
+        "daily": ["temperature_2m_max", "precipitation_sum", "wind_speed_10m_max"],
+        "timezone": "Europe/Berlin"
     }
-    response = openmeteo.weather_api(url, params=params)[0]
 
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Is still above sea level: {isStillAboveSeaLevel(response.Elevation())}")
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+    daily = response.Daily()
+
+    temperature_2m_max = getTopTenMedian(response.Daily().Variables(0).ValuesAsNumpy())
+    precipitation_sum = getTopTenMedian(response.Daily().Variables(1).ValuesAsNumpy())
+    wind_speed_10m_max = getTopTenMedian(response.Daily().Variables(2).ValuesAsNumpy())
+
+    return temperature_2m_max, precipitation_sum, wind_speed_10m_max
+
+
+def checkLivable(pLat, pLong):
+    legacy_temperature_2m_max, legacy_precipitation_sum, legacy_wind_speed_10m_max = getLegacyClimateData(pLat, pLong)
+    future_temperature_2m_max, future_precipitation_sum, future_wind_speed_10m_max = getFutureClimateData(pLat, pLong)
+
+    if(legacy_precipitation_sum / future_precipitation_sum > allowedDeviationPercentage):
+        return False
+    elif(legacy_temperature_2m_max / future_temperature_2m_max > allowedDeviationPercentage):
+        return False
+    elif(legacy_wind_speed_10m_max / future_wind_speed_10m_max > allowedDeviationPercentage):
+        return False
+
+    return True
 
 
 def getCordinates(pName):
@@ -42,21 +94,19 @@ def getCordinates(pName):
     return lat, long
 
 
-def isStillAboveSeaLevel(pLat, pLong):
+def isStillAboveSeaLevelCords(pLat, pLong):
     url = f"https://api.open-meteo.com/v1/elevation?latitude={pLat}&longitude={pLong}"
 
     # Eine GET-Anfrage an die API senden
     response = requests.get(url)
 
     elevation = response.json()['elevation'][0]
-
+    print(elevation)
     return (elevation - seaLevelRise > 1)
 
 
-def isStillAboveSeaLevel(pElevation):
+def isStillAboveSeaLevelElevation(pElevation):
     return (pElevation - seaLevelRise > 1)
 
 
-#print(f"Is still above sea level: {isStillAboveSeaLevel(latitude, longitude)}")
-#lat, long = getCordinates('Berlin')
-#print(f"Is still above sea level: {isStillAboveSeaLevel(10)}")
+print(f"is Livable: {checkLivable(24,90)}")
